9c0a371c38903201797a3fa49bd84a76e27b6054
Salah Othman <Salahothmansaad@gmail.com>
2025-07-15 20:46:58 +0300

    create Notes APP

--- project_fi/lib/main.dart
+++ project_fi/lib/main.dart
@@ -1,44 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const NotesApp());
 }
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Matrial App EX',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark(),
-      home: HomeScreen(),
-      color: Colors.amber,
-    );
-  }
-}
+class NotesApp extends StatelessWidget {
+  const NotesApp({Key? key}) : super(key: key);
 
-class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("KH"),
-        titleTextStyle: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-      ),
-      body: Container(
-        child: Text(
-          "Welcome!",
-          style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-        ),
-      ),
-      backgroundColor: const Color.fromARGB(255, 235, 178, 7),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('Welcome!');
-        },
-        child: Icon(Icons.arrow_drop_up),
-      ),
-      endDrawer: Drawer(),
-    );
+    return MaterialApp(theme: ThemeData.dark(), home: Scaffold());
   }
 }
